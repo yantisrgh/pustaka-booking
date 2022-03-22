e07d6c71354754a43af9fde343744c01aba5b97d
yantisrgh <yantisrgh15@gmail.com>
2022-03-22 15:15:15 +0700

    menambahkan folder pustaka

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Yanti Saragih
+ Saya tinggal di daerah Menceng
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
