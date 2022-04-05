a70099b05ca5c1a44024945471a3d1e52ef91dd7
yantisrgh <yantisrgh15@gmail.com>
2022-04-05 13:34:42 +0700

    Menambahkan view about pada tempalate sederhana dengan menggunakan CI

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -16,8 +16,7 @@ CodeIgniter</h3>
  </hgroup>
  <nav>
  <ul>
- <li><a href="<?php echo 
-base_url().'index.php/web' ?>">Home</a></li>
+ <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
  <li><a href="<?php echo 
 base_url().'index.php/web/about' ?>">About</a></li>
  </ul>
